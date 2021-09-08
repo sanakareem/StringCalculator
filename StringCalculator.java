adef755760996c27c9f381967f806396e856d987
sanakareem <55546183+sanakareem@users.noreply.github.com>
2021-09-08 16:03:50 +0530

    Java Implementation

---
+++ StringCalculator.java
@@ -0,0 +1,130 @@
+package com.StringCalculatorAssessment.tddtest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringCalculator {
+	  public static final void add1(final String numbers) {
+	        String[] numbersArray = numbers.split(",");
+	        if (numbersArray.length > 2) {
+	            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+	        } else {
+	            for (String number : numbersArray) {
+	                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
+	            }
+	        }
+	    }
+	  public static final int add2(final String numbers) { // Changed void to int
+	        String[] numbersArray = numbers.split(",");
+	        if (numbersArray.length > 2) {
+	            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+	        } else {
+	            for (String number : numbersArray) {
+	                if (!number.isEmpty()) {
+	                    Integer.parseInt(number);
+	                }
+	            }
+	        }
+	        return 0; // Added return
+	    }
+	  public static int add3(final String numbers) {
+		    int returnValue = 0;
+		    String[] numbersArray = numbers.split(",");
+		    if (numbersArray.length > 2) {
+		        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+		    }
+		    for (String number : numbersArray) {
+		        if (!number.trim().isEmpty()) { // After refactoring
+		            returnValue += Integer.parseInt(number);
+		        }
+		    }
+		    return returnValue;
+		}
+	  public static int add4(final String numbers) {
+		    int returnValue = 0;
+		    String[] numbersArray = numbers.split(",");
+		    // Removed after exception
+		    // if (numbersArray.length > 2) {
+		    // throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+		    // }
+		    for (String number : numbersArray) {
+		        if (!number.trim().isEmpty()) { // After refactoring
+		            returnValue += Integer.parseInt(number);
+		        }
+		    }
+		    return returnValue;
+		}
+	  public static int add5(final String numbers) {
+		    int returnValue = 0;
+		    String[] numbersArray = numbers.split(",|n"); // Added |n to the split regex
+		    for (String number : numbersArray) {
+		        if (!number.trim().isEmpty()) {
+		            returnValue += Integer.parseInt(number.trim());
+		        }
+		    }
+		    return returnValue;
+		}
+	  public static int add6(final String numbers) {
+		    String delimiter = ",|n";
+		    String numbersWithoutDelimiter = numbers;
+		    if (numbers.startsWith("//")) {
+		        int delimiterIndex = numbers.indexOf("//") + 2;
+		        delimiter = numbers.substring(delimiterIndex, delimiterIndex + 1);
+		        numbersWithoutDelimiter = numbers.substring(numbers.indexOf("n") + 1);
+		    }
+		    return add(numbersWithoutDelimiter, delimiter);
+		}
+
+		private static int add(final String numbers, final String delimiter) {
+		    int returnValue = 0;
+		    String[] numbersArray = numbers.split(delimiter);
+		    for (String number : numbersArray) {
+		        if (!number.trim().isEmpty()) {
+		            returnValue += Integer.parseInt(number.trim());
+		        }
+		    }
+		    return returnValue;
+		}
+		@SuppressWarnings("unused")
+		private static int add7(final String numbers, final String delimiter) {
+	        int returnValue = 0;
+	        String[] numbersArray = numbers.split(delimiter);
+	        List negativeNumbers = new ArrayList();
+	        for (String number : numbersArray) {
+	            if (!number.trim().isEmpty()) {
+	                int numberInt = Integer.parseInt(number.trim());
+	                if (numberInt < 0) {
+	                    negativeNumbers.add(numberInt);
+	                }
+	                returnValue += numberInt;
+	            }
+	        }
+	        if (negativeNumbers.size() > 0) {
+	            throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+	        }
+	        return returnValue;     
+	    }
+		@SuppressWarnings("unused")
+		private static int add8(final String numbers, final String delimiter) {
+            int returnValue = 0;
+            String[] numbersArray = numbers.split(delimiter);
+            List negativeNumbers = new ArrayList();
+            for (String number : numbersArray) {
+                    if (!number.trim().isEmpty()) {
+                            int numberInt = Integer.parseInt(number.trim());
+                            if (numberInt < 0) {
+                                    negativeNumbers.add(numberInt);
+                            } else if (numberInt <= 1000) {
+                                    returnValue += numberInt;
+                            }
+                    }
+            }
+            if (negativeNumbers.size() > 0) {
+                    throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+            }
+            return returnValue;                
+    }
+}
+
+
+
