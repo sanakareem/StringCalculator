49a061abdd4a0e69d9dc26c65dd0c477b373d71f
sanakareem <55546183+sanakareem@users.noreply.github.com>
2021-09-08 16:08:20 +0530

    Update StringCalculatorTest.java

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -57,7 +57,7 @@ public class StringCalculatorTest {
         try {
             StringCalculator.add6("3,-6,15,-18,46,33");
         } catch (RuntimeException e) {
-            exception = e;
+            exception = e;   
         }
         Assert.assertNotNull(exception);
         Assert.assertEquals("Negatives not allowed: [-6, -18]", exception.getMessage());
